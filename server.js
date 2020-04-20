72f36f7aea1447877b15439073d86329b916cf8a
Carlos Hernandez <carlos@devcodify.com>
2020-04-20 17:39:47 -0600

    create folder models

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const rout = require('./routes/routes.js')
 const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutDB', {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 })
 
-//routes
-app.use(require('./routes/api.js'));
+//routes from routes folder
+app.use(rout);
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`))
