2088ce72d3f59eb3266946ce91dc2a9da8f695e9
Carlos Hernandez <carlos@devcodify.com>
2020-04-22 15:56:59 -0600

    create route route.js

--- routes/route.js
+++ routes/route.js
@@ -2,3 +2,23 @@ const router = require("express").Router();
 const db = require("../models/indexModel");
 const path = require("path");
 
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+
+  router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+  
+
+  router.get("/api/workouts", (req, res) => {
+    db.Workout.find({
+
+    }).then(result => {
+        res.json(result);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
